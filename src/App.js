03cf75b13b9f64eab0df8f84a43d9b0b7d89abf4
OnlyVCS <vcsdevacc@gmail.com>
2022-03-26 20:40:19 -0300

    Body added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import Header from './components/Header';
+import Body from './components/Body';
 
 function App() {
   return (
     <>
       <Header />
-      <div>
-        App
-      </div>
+      <Body />
     </>
   );
 }
---
+++ src/components/Body/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './styles.css';
+
+const Body = () => {
+    return ( 
+        <div className='body-content'>
+        </div>
+     );
+}
+ 
+export default Body;
